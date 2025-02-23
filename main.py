ceb032e2dbafa82e45584ea9af51242249320e2f
zahra <z.durandish52@gmail.com>
2025-02-23 12:24:36 -0800

    screen and paddle objects created

---
+++ main.py
@@ -0,0 +1,27 @@
+from turtle import Screen
+from paddle import Paddle
+
+
+screen = Screen()
+screen.setup(800, 600)
+screen.bgcolor("black")
+screen.title("PONG GAME")
+screen.tracer(0)
+
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+
+
+
+screen.exitonclick()
