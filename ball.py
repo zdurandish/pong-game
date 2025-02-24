60ea8f2c61d767d110b70fefcd1fa77ab234250c
zahra <z.durandish52@gmail.com>
2025-02-24 03:52:35 -0800

    added some updates

--- ball.py
+++ ball.py
@@ -9,12 +9,22 @@ class Ball(Turtle):
         self.color("white")
         self.x_move = 10
         self.y_move = 10
+        self.move_speed = 0.1
 
     def move(self):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-    def bounce(self):
+    def y_bounce(self):
         self.y_move *= -1
+
+    def x_bounce(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.move_speed = 0.1
+        self.goto(0, 0)
+        self.x_bounce()
         
