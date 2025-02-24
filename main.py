fbfacc1508155e2c92cf77d03d1377e055bca942
zahra <z.durandish52@gmail.com>
2025-02-24 02:52:15 -0800

    collision with top and bottom wall conditions added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 
 screen = Screen()
@@ -10,6 +12,7 @@ screen.tracer(0)
 
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -19,7 +22,13 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # Detecting collision with top and bottom walls:
+    if ball.ycor() > 290 or ball.ycor() < -290:
+        ball.bounce()
 
 
 
